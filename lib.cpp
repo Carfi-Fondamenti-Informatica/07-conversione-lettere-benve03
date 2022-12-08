023a3cce6a4f8fb286e1c5e1ae798c4ec91c1033
benve03 <117894307+benve03@users.noreply.github.com>
2022-12-08 19:25:50 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,10 +1,14 @@
 #include "lib.h"
-bool convL (char&a) {
+bool angelo (char&a) {
+    if ((a >= 65 and a <= 90) or (a >= 97 and a <= 122)) {
         if (a >= 65 and a <= 90) {
             a += 32;
-        } 
-    else if (a >= 97 and a <= 122){
+        } else {
             a -= 32;
+        }
         return 1;
-    }return 0;    
+    }
+    else {
+        return 0;
+    }
 }
