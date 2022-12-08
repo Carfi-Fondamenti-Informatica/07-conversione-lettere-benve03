94fecd2fd47f6177cec56122fb6b78c478dc8c5c
benve03 <117894307+benve03@users.noreply.github.com>
2022-12-08 18:01:39 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
-
+#include "giorgia.h"
+bool convL (char&a) {
+    if ((a >= 65 and a <= 90) or (a >= 97 and a <= 122)) {
+        if (a >= 65 and a <= 90) {
+            a += 32; // sarebbe come dire a=a+32
+        } else {
+            a -= 32;
+        }
+        return 1; //vero
+    }
+    else {
+        return 0; //falso
+    }
+}
