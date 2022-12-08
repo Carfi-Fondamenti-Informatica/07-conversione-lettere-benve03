5f7d4fe02e86da324d2bb93a79a4308df330fb72
benve03 <117894307+benve03@users.noreply.github.com>
2022-12-08 18:16:37 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,11 +1,10 @@
 #include "lib.h"
 bool convL (char&a) {
-    if (a >= 65 and a <= 90) {
         if (a >= 65 and a <= 90) {
-            a += 32; // sarebbe come dire a=a+32
-        } else if (a >= 97 and a <= 122){
+            a += 32;
+        } 
+    else if (a >= 97 and a <= 122){
             a -= 32;
-        return 1; //vero
-    }return 0; //falso
-    }
+        return 1;
+    }return 0;    
 }
